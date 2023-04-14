e231d6dd8f27e64b51d1d60bb683fa902232bd74
miky3000 <mikecode808@gmail.com>
2023-04-14 19:52:24 +0200

    componente listarUsuarios

--- src/app/components/profesor/listar-usuarios/listar-usuarios.component.ts
+++ src/app/components/profesor/listar-usuarios/listar-usuarios.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/interfaces/user.interface';
+import { ProfesorService } from 'src/app/services/profesor.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-listar-usuarios',
   templateUrl: './listar-usuarios.component.html',
   styleUrls: ['./listar-usuarios.component.css']
 })
+
 export class ListarUsuariosComponent {
+  //arrUsers: User[];
+
+  constructor(private profeSV: ProfesorService) {
+
+  }
+
 
 }
