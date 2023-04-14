ea709c2327e195ad6e85cf5eccc260e16d6f38d4
miky3000 <mikecode808@gmail.com>
2023-04-14 12:41:27 +0200

    Incorporada ruta registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/usuario/login/login.component';
+import { RegistroComponent } from './components/usuario/registro/registro.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
   { path: '**', redirectTo: '' }
 ];
 
