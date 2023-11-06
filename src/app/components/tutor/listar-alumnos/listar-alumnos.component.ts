3625c215482cf0b67c0eac16fd983468712d644c
MikeJGT <117930380+MikeJGT@users.noreply.github.com>
2023-11-06 21:57:09 +0100

    Update listar-alumnos.component.ts

--- src/app/components/tutor/listar-alumnos/listar-alumnos.component.ts
+++ src/app/components/tutor/listar-alumnos/listar-alumnos.component.ts
@@ -27,27 +27,20 @@ export class ListarAlumnosComponent {
   ngOnInit() {
     this.activatedRoute.params.subscribe(async (params: any) => {
       let id = params.id
-      console.log('PARAMETROSSSS', params);
       switch (this.token.role) {
         case 'profesor': {
-          // this.arrAlumnos = await this.tutorService.getAlumnoByTutorId(id)
           this.arrAlumnos = await this.alumnoSV.getAlumnosByClaseId(id)
-          console.log('Horario para profesor', this.token.user_id);
           break;
         }
         case 'tutor': {
           this.arrAlumnos = await this.tutorService.getAlumnoByTutorId(id)
-          console.log('Horario para padres', this.token.user_id);
           break;
         }
         default: {
-          console.log('No hay horario para tu rol');
           break;
         }
       }
     })
-    console.log(this.arrAlumnos);
-
   }
 
 }
