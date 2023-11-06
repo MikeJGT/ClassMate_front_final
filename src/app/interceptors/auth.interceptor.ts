f7fac5870a5d370a89b78aa1afb84a692f233692
MikeJGT <117930380+MikeJGT@users.noreply.github.com>
2023-11-06 21:59:35 +0100

    Update auth.interceptor.ts

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -12,7 +12,6 @@ export class AuthInterceptor implements HttpInterceptor {
 
   constructor() { }
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    console.log('pasa por interceptors')
     //Sacar token de localStorage
     const token = localStorage.getItem('token') || '';
     //Insertar token en angular
